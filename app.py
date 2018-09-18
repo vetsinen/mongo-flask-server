31cbb3b31dda656a87d5b6d6d63755b89fb7f279
vetsinen <vetsinen@gmail.com>
2018-09-19 02:10:41 +0300

    locally gets data

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
-
-from pymongo import MongoClient
-import urllib
-
-client = MongoClient('mongodb://larinsergey101:'+urllib.parse.quote('1r1nalar1n@')+'@ds259742.mlab.com:59742/vuevents')
-#client = MongoClient('mongodb://heroku_knlkljm8:r6ko4kpeg5bqbr76e8edoogt9o@ds261302.mlab.com:61302/heroku_knlkljm8')
-db = client["vuevents"]
-events = db["events"]
+from flask_pymongo import PyMongo
 
 application = Flask(__name__)
 CORS(application)
 
-import datetime
+import urllib
+application.config["MONGO_URI"] = 'mongodb://larinsergey101:'+urllib.parse.quote('1r1nalar1n@')+'@ds259742.mlab.com:59742/vuevents'
+mongo = PyMongo(application)
 
+import datetime
 nowstr = str(datetime.datetime.now())[:10]
 
 
 @application.route('/')
 def status():
-    return str(type(events))
+    d = mongo.db.events.count()
+    return 'hello '+str(d)
 
 
 @application.route('/events')
--- eventsdb.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-import urllib
-
-# client = MongoClient('mongodb://larinsergey101:'+urllib.parse.quote('1r1nalar1n@')+'@ds259742.mlab.com:59742/vuevents')
-client = MongoClient('mongodb://heroku_knlkljm8:r6ko4kpeg5bqbr76e8edoogt9o@ds261302.mlab.com:61302/heroku_knlkljm8')
-db = client["vuevents"]
-events = db["events"]

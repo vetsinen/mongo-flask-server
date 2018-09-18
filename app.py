38234bb643622e6d5ed36977325390354883e079
vetsinen <vetsinen@gmail.com>
2018-09-18 22:52:21 +0300

    mongo and cors dependencies added

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+import datetime
+nowstr = str(datetime.datetime.now())[:10]
+
+
+@application.route('/')
+def status():
+    return 'Hello,current date is: '+ nowstr
+
+@application.route('/events')
+@application.route('/events/<string:date>')
+def events(date=nowstr):
+    return jsonify(zzzzzzzz(date))

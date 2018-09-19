dd0165f0e6e6b9712886212248dbc6a03805660c
vetsinen <vetsinen@gmail.com>
2018-09-19 11:15:56 +0300

    experiments

--- tst.py
+++ tst.py
@@ -3,7 +3,16 @@ import urllib
 
 client = MongoClient('mongodb://larinsergey101:'+urllib.parse.quote('1r1nalar1n@')+'@ds259742.mlab.com:59742/vuevents')
 db = client["vuevents"]
-events = db["events"]
+test = db["test"]
 from pprint import pprint
 
-pprint((events.find_one({'datestr':'2018-10-10'})))
+if test.find({"_id":"33"}).count()>0:
+    print(42)
+if not test.find({"_id":"2"}):
+    test.insert({"_id": "2", "a": "b"})
+# test.insert({"_id":"2","a":"b"})
+
+
+
+for x in test.find():
+    pprint(x)
